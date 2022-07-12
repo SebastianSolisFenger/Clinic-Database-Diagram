ae078b317948a219eb8dba83c992e97800401619
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-12 09:54:10 -0300

    Create database and basic architecture of its tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,46 @@
 
+CREATE DATABASE Clinic_Database_Diagram
+
+CREATE TABLE patients (
+    id INT GENERATED ALWAYS AS IDENTITY
+    name VARCHAR(100)
+    date_of_birth DATE
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY
+    admitted_at TIMESTAMP
+    patient_id INT
+    status VARCHAR(100)
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE treatments (
+    id INT GENERATED ALWAYS AS IDENTITY
+    type VARCHAR(100)
+    name VARCHAR(100)
+    PRIMARY KEY (id)
+)
+
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY
+    total_amount DECIMAL
+    generated_at TIMESTAMP
+    payed_at TIMESTAMP
+    medical_history_id INT
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE invoice_items (
+    id INT GENERATED ALWAYS AS IDENTITY
+    unit_price DECIMAL
+    quantity INT
+    total_price DECIMAL
+    invoice_id INT
+    treatment_id INT
+    PRIMARY KEY (id)
+)
+
+
